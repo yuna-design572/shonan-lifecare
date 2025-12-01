1b552671bcbd95ab1143d46f81967cab265efe3d
byyuna <yuna.design572@gmail.com>
2025-12-02 04:32:48 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -19,3 +19,23 @@ ham.addEventListener('click', () => {
   ham.classList.toggle('active');
   nav.classList.toggle('active');
 });
+
+const fadeInElements = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, { threshold: 0.1 });
+
+fadeInElements.forEach(el => {
+  observer.observe(el);
+
+  // 画面内にすでにある場合の対策
+  const rect = el.getBoundingClientRect();
+  if (rect.top < window.innerHeight) {
+    el.classList.add('show');
+  }
+});
